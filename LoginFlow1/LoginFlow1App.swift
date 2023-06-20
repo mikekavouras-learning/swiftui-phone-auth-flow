26deec3558fa9052de9398e1a0b415d195c85cdf
Mike Kavouras <kavourasm@gmail.com>
2023-06-20 14:54:26 -0400

    Initial Commit

---
+++ LoginFlow1/LoginFlow1App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFlow1App.swift
+//  LoginFlow1
+//
+//  Created by Mike Kavouras on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginFlow1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
